749328edbfa5ddc15efa509f327db20680aa31f5
zreinaldon <zreinaldon@gmail.com>
2023-09-01 11:24:50 -0300

    feat: filtrar as informações o que o usuário deseja mostrar do personagem escolhido

--- script.js
+++ script.js
@@ -3,19 +3,43 @@ const searchBtn = document.getElementById('search-button');
 const containerCharacter = document.getElementById('container-character');
 const imgCharacter = document.getElementById('img-character');
 
-const fetchApi = (value) => {
-    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
-    .then((res) => (res.json()))
-    .then((data) => {
-        return data;
-    })
-
+async function fetchF(value) {
+    const result = await fetch(`https://rickandmortyapi.com/api/character/${value}`)
+    .then((res) => res.json());
+    
     return result;
 }
 
+const filterData = (result) => {
+    const divFilter = document.getElementsByClassName('filter');
+    const filterArray = Array.from(divFilter);
+
+    const filteredObject = {};
+
+    filterArray.map((element) => {
+        return document.getElementById(element.lastElementChild.id);
+    }).map((element) => {
+        if(element.checked) {
+            console.log(element);
+            filteredObject[element.name] = result[element.name];
+        }
+    })
+
+    return filteredObject;
+}
+
+
+
 searchBtn.addEventListener('click', async () => {
-    const result = await fetchApi(inputValue.value);
-    containerCharacter.innerText = JSON.stringify(result, undefined, 1);
+    const result = await fetchF(inputValue.value);
 
     imgCharacter.setAttribute('src', result.image);
+
+    //containerCharacter.innerText = JSON.stringify(result, undefined, 1); //mostra todas as propriedades do objeto sem filtro 
+    containerCharacter.innerText = JSON.stringify(filterData(result), undefined, 1);
 })
+
+
+
+
+
